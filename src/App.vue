6047c13201207e339c155ab1989ac6420b3a6380
Richard Wong <ricwtk@gmail.com>
2026-05-29 17:41:30 +0800

    Update App.vue (display zone show/hide in mobile)

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ const layoutStore = useLayoutStore()
     </div>
     <div class="p-2 pt-1 flex flex-row gap-2 grow overflow-hidden">
       <ReadingList class="flex-1 md:block md:w-90" :class="[layoutStore.showDisplayZone ? 'hidden' : 'block']"></ReadingList>
-      <DisplayZone class="flex-1"></DisplayZone>
+      <DisplayZone class="flex-1 md:block" :class="[layoutStore.showDisplayZone ? 'block' : 'hidden']"></DisplayZone>
     </div>
   </div>
 </template>
